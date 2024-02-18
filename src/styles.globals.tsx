b1f350f5c729d3e6fee442be85df77da47b63731
jayson <jayson@helium>
2024-02-18 15:34:39 -0500

    feature: adds css preflight and initial global styles to the project, including the main App font: quicksand

---
+++ src/styles.globals.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    :root{
+        font-family: "Quicksand", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 400;
+        font-style: normal;
+    }
+`;
