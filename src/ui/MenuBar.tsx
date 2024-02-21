c25847d467cc097bfafa2a622c9e62c533aedeae
jayson <jayson@helium>
2024-02-21 17:04:47 -0500

    feat: implements MenuBar for mobile clients

--- src/ui/MenuBar.tsx
+++ src/ui/MenuBar.tsx
@@ -1,8 +1,93 @@
-import { useLocation } from 'react-router-dom';
+import { useApplicationContext } from '@/context/useApplicationContext';
+import { Flex } from '@/ui/Flex';
+import { IconContext } from 'react-icons';
+import {
+  IoAddOutline,
+  IoArrowBackOutline,
+  IoCreateOutline,
+  IoMenu,
+  IoMoonOutline,
+  IoSunnyOutline,
+} from 'react-icons/io5';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import styled, { useTheme } from 'styled-components';
 
-export default function MenuBar() {
+const MenuBarContainer = styled(Flex.Row)`
+  justify-content: flex-end;
+  padding: 1.25rem;
+
+  & > button:first-child {
+    margin-right: auto;
+  }
+`;
+
+const MenuIconButton = styled.button`
+  border: none;
+`;
+
+function BackMenuIcon() {
+  const navigate = useNavigate();
+  const goBack = () => navigate(-1);
+
+  return (
+    <MenuIconButton onClick={goBack}>
+      <IoArrowBackOutline />
+    </MenuIconButton>
+  );
+}
+
+function DrawerMenuIcon() {
+  return (
+    <MenuIconButton>
+      <IoMenu />
+    </MenuIconButton>
+  );
+}
+
+function ThemeSwitcherMenuIcon() {
+  const { isDarkTheme, toggleTheme } = useApplicationContext();
+
+  return <MenuIconButton onClick={toggleTheme}>{isDarkTheme ? <IoSunnyOutline /> : <IoMoonOutline />}</MenuIconButton>;
+}
+
+function ContextualMenuIcon() {
   const location = useLocation();
 
+  switch (location.pathname) {
+    case '/app/manage-vocabulary/add-word':
+      return (
+        <MenuIconButton>
+          <Link to="/app/manage-vocabulary/edit-word">
+            <IoCreateOutline />
+          </Link>
+        </MenuIconButton>
+      );
+    case '/app/manage-vocabulary/edit-word':
+      return (
+        <MenuIconButton>
+          <Link to="/app/manage-vocabulary/add-word">
+            <IoAddOutline />
+          </Link>
+        </MenuIconButton>
+      );
+
+    default:
+      return null;
+  }
+}
+
+export default function MenuBar() {
+  const theme = useTheme();
+
   console.log(location);
-  return <p>MenuBar placeholder</p>;
+  return (
+    <IconContext.Provider value={{ size: '1.5rem', color: theme.primaryTextColor }}>
+      <MenuBarContainer $gap="1rem">
+        <BackMenuIcon />
+        <ThemeSwitcherMenuIcon />
+        <ContextualMenuIcon />
+        <DrawerMenuIcon />
+      </MenuBarContainer>
+    </IconContext.Provider>
+  );
 }
