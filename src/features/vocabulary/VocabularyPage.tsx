467ac51bc968a9fc49a80351e62daa05ea906f3c
jayson <jayson@helium>
2024-04-21 15:23:18 -0500

    refactor: moving supporting components to their own file

--- src/features/vocabulary/VocabularyPage.tsx
+++ src/features/vocabulary/VocabularyPage.tsx
@@ -1,16 +1,12 @@
 import { FilterPill, FilterRow } from '@/features/vocabulary/DraggableFilterRow';
 import SearchBar from '@/features/vocabulary/SearchBar';
+import VocabularyWordListItem from '@/features/vocabulary/VocabularyWordListItem';
 import useVocabularWords from '@/features/vocabulary/useVocabularWords';
-import { VocabularyWord, WordType } from '@/types/domainTypes';
 import { Flex } from '@/ui/Flex';
 import PageTitle from '@/ui/PageTitle';
-import { SpinningIcon } from '@/ui/SpinningIcon';
 import { useIntersection } from '@mantine/hooks';
-import { MouseEventHandler, PropsWithChildren, forwardRef, useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
-import { IconContext } from 'react-icons';
-import { IoVolumeHighOutline } from 'react-icons/io5';
-import { PiCircleDashedThin } from 'react-icons/pi';
 import { useSearchParams } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -22,130 +18,6 @@ const PageContainer = styled(Flex.Column)`
 const PageBody = styled(Flex.Column)`
   gap: 1rem;
 `;
-
-const PlayButtonContainer = styled.button<{ $audioStatus: 'playing' | 'idle' }>`
-  width: 2rem;
-  height: 2rem;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  background-color: ${(props) => ('idle' == props.$audioStatus ? 'orange' : 'inherit')};
-  border-radius: 4rem;
-  & > div {
-    position: absolute;
-    /* z-index: -1; */
-  }
-`;
-
-type VocabularyPlayButtonProps = {
-  audioSource: string[];
-};
-const VocabularyPlayButton = (props: VocabularyPlayButtonProps) => {
-  const audioRef = useRef<HTMLAudioElement | null>(null);
-  const [audioStatus, setAudioStatus] = useState<'idle' | 'playing'>('idle');
-
-  const onPlayAudio: MouseEventHandler = (event) => {
-    event.stopPropagation();
-    const audioElement = audioRef.current;
-    if (!audioElement) return;
-    audioElement.load;
-    audioElement.paused ? audioElement.play() : audioElement.pause();
-  };
-
-  useEffect(() => {
-    const audio = audioRef.current;
-    if (!audio) return;
-
-    const signalPlayStarted = (event) => {
-      setAudioStatus('playing');
-      console.log(event);
-    };
-    const signalIdle = (event) => {
-      setAudioStatus('idle');
-      console.log(event);
-    };
-    audio.addEventListener('play', signalPlayStarted);
-    audio.addEventListener('ended', signalIdle);
-
-    return () => {
-      audio.removeEventListener('play', signalPlayStarted);
-      audio.removeEventListener('ended', signalIdle);
-    };
-  }, []);
-
-  return (
-    <IconContext.Provider value={{ size: '2.5rem', color: 'orange' }}>
-      <audio ref={audioRef} preload="none">
-        {props.audioSource.map((sourceURL) => {
-          if (sourceURL.endsWith('.mp3')) {
-            return <source key={sourceURL} src={sourceURL} type="audio/mpeg" />;
-          }
-
-          if (sourceURL.endsWith('.ogg')) {
-            return <source key={sourceURL} src={sourceURL} type="audio/ogg" />;
-          }
-
-          return null;
-        })}
-      </audio>
-      <PlayButtonContainer onClick={onPlayAudio} $audioStatus={audioStatus}>
-        <IoVolumeHighOutline size={'1.25rem'} color={'idle' == audioStatus ? 'white' : 'orange'} />
-        <SpinningIcon>
-          <PiCircleDashedThin style={{ display: 'playing' == audioStatus ? 'block' : 'none' }} />
-        </SpinningIcon>
-      </PlayButtonContainer>
-    </IconContext.Provider>
-  );
-};
-
-const ListItem = styled.article`
-  display: flex;
-  flex-direction: row;
-  border-radius: 0.5rem;
-  border: 1px solid ${(props) => props.theme.borderColor};
-  justify-content: space-between;
-  padding: 1rem;
-  background-color: ${(props) => props.theme.sectionBg};
-`;
-const LeftInfo = styled(Flex.Column)`
-  & > p:first-child {
-    font-weight: 600;
-    font-size: 1.125rem;
-  }
-
-  & > p:last-child {
-    /* white-space: wrap; */
-    text-overflow: ellipsis;
-  }
-`;
-const RightInfo = styled(Flex.Column)`
-  align-items: center;
-  justify-content: center;
-  & > p:last-child {
-    font-weight: 600;
-    font-size: 0.875rem;
-  }
-`;
-
-type VocabularyWordListItemProps = {
-  word: VocabularyWord;
-};
-
-const VocabularyWordListItem = forwardRef<HTMLElement, VocabularyWordListItemProps>(function listItem({ word }, ref) {
-  return (
-    <ListItem ref={ref}>
-      <LeftInfo>
-        <p>{word.word}</p>
-        <p>{word.englishTranslations.join(', ')}</p>
-      </LeftInfo>
-      <RightInfo>
-        <VocabularyPlayButton audioSource={word.recordingURLs} />
-        <p>{word.pronunciations[0] || '[pending]'}</p>
-      </RightInfo>
-    </ListItem>
-  );
-});
 
 export default function VocabularyPage() {
   const [searchText, setSearchText] = useState('');
